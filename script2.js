414b8f23c85152f9c73ad382ee88c003aa72b82e
Evgeniya Malyushkina <e.malyushkina@mail.ru>
2020-12-14 20:30:34 +0800

    Задача 20

--- script2.js
+++ script2.js
@@ -34,3 +34,14 @@ console.log(toCamelCase(str19));
 
 // 20. Сделайте функцию inArray, которая определяет, есть в массиве элемент с заданным текстом или нет.
 // Функция первым параметром должна принимать текст элемента, а вторым - массив, в котором делается поиск. Функция должна возвращать true или false.
+let myArr20 = [15, 4, 59, 24, 11];
+
+function inArray(text, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == text) {
+            return true;
+        }
+    };
+return false;
+}
+console.log(inArray(23, myArr20));
